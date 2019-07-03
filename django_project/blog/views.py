8616566384968fe94af33b8590659c22219072d7
electricSamarth <samarthmathur13@gmail.com>
2019-07-03 16:11:37 +0530

    created a base html file so that it becomes easy to make changes and new html pages which are created are uniform and are less cumbersome to make(type)

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+	{
+	'author': 'Samarth Mathur',
+	'title':'Blog Post 1',
+	'content':'First post content',
+	'date_posted':'August 27,2018'
+	},
+	{
+	'author': 'Sama',
+	'title':'Blog Post 2',
+	'content':'second post content',
+	'date_posted':'August 28,2018'
+	}
+
+]
 
 def home(request):
-	return render(request,'blog/home.html')
+	context={
+		'posts':posts
+	}
+	return render(request,'blog/home.html',context)
 
 def about(request):
-	return render(request,'blog/about.html')	
+	return render(request,'blog/about.html',{'title':'About'})	
